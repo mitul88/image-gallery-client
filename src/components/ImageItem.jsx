f7ea7a13c9d7d49b01ccb849306140ce1f4ef67a
shahed <shahed.rahman88@gmail.com>
2023-09-06 15:28:56 +0600

    image details layout done

---
+++ src/components/ImageItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BiLike, BiCommentDetail } from "react-icons/bi";
+
+const ImageItem = () => {
+  return (
+    <div className='rounded-md h-[300px] max-w-[300px] mx-auto bg-white flex flex-col'>
+        <img 
+            className="h-[200px] rounded-t-md" 
+            src="https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?cs=srgb&dl=pexels-james-wheeler-417074.jpg&fm=jpg" 
+            alt="" 
+        />
+        <div className='h-[100px] px-2'>
+            <h4 className='text-xl my-2'>Image title</h4>
+            <div className="flex flex-row justify-between w-full">
+                <div className='rounded-lg bg-gray-200 p-2 text-sm font-bold text-gray-500 flex flex-row items-center'><BiLike className='mr-2' /> 22</div>
+                <div className='rounded-lg bg-gray-200 p-2 text-sm font-bold text-gray-500 flex flex-row items-center'><BiCommentDetail className='mr-2' /> 8</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ImageItem
--- src/pages/ImageDetails.jsx
+++ src/pages/ImageDetails.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
+import ImageItem from '../components/ImageItem'
 
 const ImageDetailsPage = () => {
   return (
     <section className='bg-slate-200 md:min-h-screen py-2'>
-      <div className='container mx-auto h-[600px] flex flex-col md:flex-row gap-2'>
-        <div className='w-full md:w-2/3 bg-red-100 h-full rounded-md'></div>
-        <div className='w-full md:w-1/3 bg-red-400 h-full rounded-md'></div>
+      <div className='container mx-auto lg:h-[600px] flex flex-col lg:flex-row gap-2'>
+        <div className='w-full lg:w-2/3 h-full sm:rounded-md'>
+        <img 
+            className="h-full w-fit mx-auto sm:rounded-md" 
+            src="https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?cs=srgb&dl=pexels-james-wheeler-417074.jpg&fm=jpg" 
+            alt="" 
+        />
+        </div>
+        <div className='w-full lg:w-1/3 bg-white h-full rounded-md'></div>
       </div>
-      <h2 className='container mx-auto text-2xl'>More photos like this</h2>
-      <div>
-
+      <h2 className='container mx-auto font-bold text-3xl text-center md:text-left my-5'>More photos like this</h2>
+      <div className='container mx-auto lg:mb-5'>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
+          <ImageItem />
+          <ImageItem />
+          <ImageItem />
+          <ImageItem />
+          <ImageItem />
+        </div>
       </div>
     </section>
   )
