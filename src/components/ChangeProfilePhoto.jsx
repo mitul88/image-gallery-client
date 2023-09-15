c658c33d766001b6e7563fc46a1f623991896e20
shahed <shahed.rahman88@gmail.com>
2023-09-15 21:09:57 +0600

    conditional change photo added

---
+++ src/components/ChangeProfilePhoto.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import ImageUploader from './ImageUploader'
+
+const ChangeProfilePhoto = () => {
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const submitForm = e => {
+        e.preventDefault()
+        const formData = new FormData();
+        formData.append("image", selectedFile);
+        console.log(formData)
+      };
+
+  return (
+    <form onSubmit={submitForm} className='m-5 p-5 rounded-md border border-gray-200'>
+        <ImageUploader onFileSelect={(file)=> setSelectedFile(file)} />
+        <button className="px-5 py-1 rounded-sm bg-green-500 text-xs text-white tracking-wider">Change</button>
+    </form>
+  )
+}
+
+export default ChangeProfilePhoto
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ChangeProfilePhoto from '../components/ChangeProfilePhoto'
 
 const Settings = () => {
+  const [showChangePhoto, setShowChangePhoto] = useState(false);
   return (
     <div>
-      <div className=''>
-        <div className='m-5 p-5 w-96 shadow-md shadow-gray-200'>
-          <button className="px-5 py-2 border border-green-500 text-sm text-green-500 rounded-md">Change Profile pic</button>
-          <p className="mt-2 text-sm text-gray-500">Change your profile pic. Your old profile photo will be deleted autometically if you choose to upload a new one.</p>
+      <div className='flex flex-col lg:flex-row flex-wrap'>
+        <div className='m-3 p-5 md:w-96 shadow-md shadow-gray-200'>
+          <button className="px-5 py-2 border border-green-500 text-sm text-green-500 rounded-md" onClick={()=>setShowChangePhoto(!showChangePhoto)}>Change Profile pic</button>
+          <p className="m-2 text-sm text-gray-500">Change your profile pic. Your old profile photo will be deleted autometically if you choose to upload a new one.</p>
+          {showChangePhoto && <ChangeProfilePhoto />}
         </div>
-        <div className='m-5 p-5 w-96 shadow-md shadow-gray-200'>
+        <div className='m-3 p-5 md:w-96 shadow-md shadow-gray-200'>
           <button className="px-5 py-2 border border-red-500 text-sm text-red-500 rounded-md">Delete Profile pic</button>
           <p className="mt-2 text-sm text-gray-500">Select this option if you choose to delete your current photo without uploading a new one.</p>
         </div>
-        <div className='m-5 p-5 w-96 shadow-md shadow-gray-200'>
+        <div className='m-3 p-5 md:w-96 shadow-md shadow-gray-200'>
         <button className="px-5 py-2 text-white text-sm bg-green-500 rounded-md">Edit Profile</button>
+          <p className="mt-2 text-sm text-gray-500">Edit your profile information.</p>
         </div>
       </div>
     </div>
