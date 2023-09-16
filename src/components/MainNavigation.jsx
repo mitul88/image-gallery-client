7fde4f4ceb23eaa6d462814414151052e4310eaa
shahed <shahed.rahman88@gmail.com>
2023-09-17 01:26:52 +0600

    home page designed

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -4,13 +4,13 @@ import { Form, Link, useRouteLoaderData } from 'react-router-dom'
 const MainNavigation = () => {
   const token = useRouteLoaderData('root');
   return (
-    <header className='w-full h-[60px] bg-sky-950 text-white flex justify-between items-center px-[80px]'>
-      <div>Image Gallery</div>
+    <header className='w-full h-[80px] bg-sky-950 text-white flex justify-between items-center px-[80px]'>
+      <div className='text-2xl font-bold'>Image Gallery</div>
       <nav className={`flex justify-${token ? 'between' : 'end'} items-center min-w-[250px]`}>
         {token && <Link to='/profile'>Profile</Link>}
         {token && <Link to='/'>Home</Link>}
         {token ? 
-          <Form action='/logout' method='post'><button to='/logout'>Logout</button></Form> : <Link to='auth?mode=login' className='px-5 py-2 rounded-lg bg-green-600'>login</Link>
+          <Form action='/logout' method='post'><button to='/logout'>Logout</button></Form> : <Link to='auth?mode=login' style={{fontFamily: 'Quicksand'}} className='px-5 py-1 rounded-lg bg-sky-900 md:shadow-lg md:shadow-sky-950 border border-sky-900 ease-in duration-150 hover:bg-sky-900/75 tracking-widest text-lg'>Signin</Link>
         }
       </nav>
     </header>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import ImageList from '../components/ImageList';
 
 const HomePage = () => {
   return (
-    <div className='bg-slate-200 md:min-h-screen'>
+    <div className='bg-neutral-300 md:min-h-screen bg-gradient-to-r from-neutral-300 via-neutral-200 to-neutral-400'>
       <HeroSection />
       <main className="container mx-auto">
         {/* <ImageList /> */}
