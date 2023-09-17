7255524dbdc6f2b868ba83a225e2cb5a9c1b10be
shahed <shahed.rahman88@gmail.com>
2023-09-17 23:52:24 +0600

    fetch user method added

--- src/utils/http.js
+++ src/utils/http.js
@@ -11,3 +11,18 @@ export const fetchRepositories = async (page=1) => {
      )
      return response.json()
 }
+
+export async function fetchUser({ id, signal }) {
+    const response = await fetch(`http://localhost:4000/api/user/${id}`, { signal });
+  
+    if (!response.ok) {
+      const error = new Error('An error occurred while fetching the user');
+      error.code = response.status;
+      error.info = await response.json();
+      throw error;
+    }
+  
+    const { event } = await response.json();
+  
+    return event;
+  }
