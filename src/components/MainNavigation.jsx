f17fe1d6ebeb4054d6f8a7a7e5ea7dc4cbeafd36
shahed <shahed.rahman88@gmail.com>
2023-09-17 13:42:22 +0600

    jwt decode added

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import { Form, Link, useRouteLoaderData } from 'react-router-dom'
+import React from 'react';
+import { Form, Link, useRouteLoaderData } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
+
 
 const MainNavigation = () => {
   const token = useRouteLoaderData('root');
+  
+  let decoded;
+  if (token) decoded = jwtDecode(token);
+  
   return (
     <header className='w-full h-[80px] bg-sky-950 text-white flex justify-between items-center px-[80px]'>
       <div className='text-2xl font-bold'>Image Gallery</div>
-      <nav className={`flex justify-${token ? 'between' : 'end'} items-center min-w-[250px]`}>
+      <nav className={`flex justify-${token ? 'between' : 'end'} items-center min-w-[250px]`} style={{fontFamily: 'Quicksand'}}>
         {token && <Link to='/'>Home</Link>}
-        {token && <Link to='/profile'>Profile</Link>}
+        {token && <Link to={`${decoded._id}/profile`}>Profile</Link>}
         {token && <Link to='/image'>Image</Link>}
         {token ? 
-          <Form action='/logout' method='post'><button to='/logout'>Logout</button></Form> : <Link to='auth?mode=login' style={{fontFamily: 'Quicksand'}} className='px-5 py-1 rounded-lg bg-sky-900 md:shadow-lg md:shadow-sky-950 border border-sky-900 ease-in duration-150 hover:bg-sky-900/75 tracking-widest text-lg'>Signin</Link>
+          <Form action='/logout' method='post'><button to='/logout'>Logout</button></Form> : <Link to='auth?mode=login'  className='px-5 py-1 rounded-lg bg-sky-900 md:shadow-lg md:shadow-sky-950 border border-sky-900 ease-in duration-150 hover:bg-sky-900/75 tracking-widest text-lg'>Signin</Link>
         }
       </nav>
     </header>
