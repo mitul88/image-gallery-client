bb1b6502b3cceea90b44b00e8c77659f457aa906
shahed <shahed.rahman88@gmail.com>
2023-09-25 23:35:12 +0600

    settings page layout changed

--- src/components/ChangeProfilePhoto.jsx
+++ src/components/ChangeProfilePhoto.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ImageUploader from './shared/ImageUploader';
 
-const ChangeProfilePhoto = () => {
+const ChangeProfilePhoto = ({showUploadForm}) => {
     const [selectedFile, setSelectedFile] = useState(null);
 
     const submitForm = e => {
@@ -12,9 +12,13 @@ const ChangeProfilePhoto = () => {
       };
 
   return (
-    <form onSubmit={submitForm} className='m-5 p-5 rounded-md border border-gray-200'>
+    <form onSubmit={submitForm} className='p-5 rounded-md border border-gray-200'>
+      <h3 className="text-xl text-center mb-2">Upload</h3>
         <ImageUploader onFileSelect={(file)=> setSelectedFile(file)} />
-        <button className="px-5 py-1 rounded-sm bg-green-500 text-xs text-white tracking-wider">Change</button>
+        <div className="w-full flex">
+          <button type='submit' className="px-3 py-1 rounded-md bg-green-500 text-xs text-white tracking-wider text-xs mr-2 font-bold tracking-widest">Change</button>
+          <button onClick={()=>showUploadForm(false)} className="px-3 py-1 rounded-md bg-white border border-red-500 text-red-500 tracking-wider text-xs">Cancel</button>
+        </div>
     </form>
   )
 }
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -9,20 +9,51 @@ const Settings = () => {
   }
   return (
     <div>
-      <div className=''>
-        <div className='m-3 p-5 md:w-96 shadow-md shadow-gray-200'>
-          <button className="px-5 py-2 border border-green-500 text-sm text-green-500 rounded-md" onClick={()=>setShowChangePhoto(!showChangePhoto)}>Change Profile pic</button>
-          <p className="m-2 text-sm text-gray-500">Change your profile pic. Your old profile photo will be deleted autometically if you choose to upload a new one.</p>
-          {showChangePhoto && <ChangeProfilePhoto />}
+      <div className='p-3 m-3 rounded shadow-md shadow-gray-200'>
+
+        <div className='m-3 p-5 flex flex-col md:flex-row justify-between items-center border-b border-gray-200'>
+          <div>
+            <button className="px-2 text-sm lg:px-5 lg:py-2 text-blue-400 rounded-md hover:bg-sky-100 ease-in duration-300">Edit Profile</button>
+          </div>
+          <div className='w-[350px]'>
+            <p className="mt-2 text-sm text-gray-500">Edit your profile information.</p>
+          </div>
         </div>
-        <div className='m-3 p-5 md:w-96 shadow-md shadow-gray-200'>
-          <button className="px-5 py-2 border border-red-500 text-sm text-red-500 rounded-md" onClick={handleDeletePhoto}>Delete Profile pic</button>
-          <p className="mt-2 text-sm text-gray-500">Select this option if you choose to delete your current photo without uploading a new one.</p>
+
+        <div className='m-3 p-5 flex flex-col md:flex-row justify-between items-center border-b border-gray-200'>
+          {!showChangePhoto && (
+            <div>
+              <button className="px-2 text-sm lg:px-5 lg:py-2 text-blue-400 rounded-md hover:bg-sky-100 ease-in duration-300" onClick={()=>setShowChangePhoto(!showChangePhoto)}>Change Profile pic</button>
+            </div>
+          )}
+          {showChangePhoto && (
+            <div>
+              <ChangeProfilePhoto showUploadForm={setShowChangePhoto} />
+            </div>
+          )}
+          <div className="w-[350px]">
+            <p className="text-sm text-gray-500"><span className="text-yellow-500 text-md font-semibold">Warning !!!</span><br /> Change your profile pic. Your old profile photo will be deleted autometically if you choose to upload a new one.</p>
+          </div>
         </div>
-        <div className='m-3 p-5 md:w-96 shadow-md shadow-gray-200'>
-        <button className="px-5 py-2 text-white text-sm bg-green-500 rounded-md">Edit Profile</button>
-          <p className="mt-2 text-sm text-gray-500">Edit your profile information.</p>
+
+        <div className='m-3 p-5 flex flex-col md:flex-row justify-between items-center border-b border-gray-200'>
+          <div>
+            <button className="px-2 text-sm lg:px-5 lg:py-2 text-blue-400 rounded-md hover:bg-sky-100 ease-in duration-300" onClick={handleDeletePhoto}>Delete Profile pic</button>
+          </div>
+          <div className="w-[350px]">
+            <p className="text-sm text-gray-500"><span className="text-yellow-500 text-md font-semibold">Warning !!!</span> <br /> Select this option if you choose to delete your current photo without uploading a new one. Your current photo will be removed permanently.</p>
+          </div>
         </div>
+
+        <div className='m-3 p-5 flex flex-col md:flex-row justify-between items-center'>
+          <div>
+            <button className="px-2 text-sm lg:px-5 lg:py-2 text-red-500 bg-red-100 rounded-md hover:bg-red-200 hover:text-red-600 ease-in duration-300" onClick={handleDeletePhoto}>Delete Acount</button>
+          </div>
+          <div className="w-[350px]">
+            <p className="text-sm text-gray-500"><span className="text-red-500 text-md font-semibold">Danger !!!</span> <br /> Your account will be deleted permanently along with all other data associated to your account.</p>
+          </div>
+        </div>
+
       </div>
     </div>
   )
