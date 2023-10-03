f501d2088b96ec44fb7ee9d95a849b7922f5a3fe
shahed <shahed.rahman88@gmail.com>
2023-10-03 13:00:23 +0600

    small screen nav menu done

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -22,7 +22,7 @@ const MainNavigation = () => {
   };
   
   return (
-    <header className={`w-full h-auto py-5 md:py-auto md:h-[80px] bg-sky-950 text-white flex ${token ? 'flex-col' : 'flex-row'} gap-5 md:gap-0 md:flex-row justify-between items-center px-[80px]`}>
+    <header className={`relative w-full h-auto py-5 md:py-auto md:h-[80px] bg-sky-950 text-white flex ${token ? 'flex-row' : 'flex-row'} gap-5 md:gap-0 md:flex-row justify-between items-center px-[80px]`}>
       <Link to="/" className='text-2xl font-bold'>
         Image <span className='text-green-500'>Gallery!</span>
       </Link>
@@ -37,17 +37,17 @@ const MainNavigation = () => {
           <button
             onClick={toggleNavMenu}  
             className='p-3 border-none rounded-full text-md tracking-wide flex flex-row justify-end items-center md:hidden bg-sky-900 border border-sky-900'
-          ><BiMenu />
+          ><AiOutlineClose />
           </button>
         ) : (
           <button
             onClick={toggleNavMenu}  
             className='p-3 border-none rounded-full text-md tracking-wide flex flex-row justify-end items-center md:hidden bg-sky-900 border border-sky-900'
-          ><AiOutlineClose />
+          ><BiMenu />
           </button>
         )
       )}
-      <nav className={`${token ? 'flex' : 'hidden'} flex-col gap-3 md:gap-0 md:flex-row flex justify-${token ? 'between' : 'end'} items-center min-w-[300px]`} style={{fontFamily: 'Quicksand'}}>
+      <nav className={`hidden ${token ? 'md:flex' : 'md:hidden'} flex-col gap-3 md:gap-0 md:flex-row flex justify-${token ? 'between' : 'end'} items-center min-w-[300px]`} style={{fontFamily: 'Quicksand'}}>
         {token && <Link to='/' className='font-bold tracking-widest'>Home</Link>}
         {token && <Link to={`${decoded?._id}/profile`} className='font-bold tracking-widest'>Profile</Link>}
         {token ? 
@@ -61,6 +61,22 @@ const MainNavigation = () => {
               ><BiLogInCircle className='md:mr-2' />Signin</Link>
         }
       </nav>
+      {showNavMenu && (
+        <nav className={`absolute top-20 right-0 bg-sky-950 z-50 py-20 ${token ? 'flex' : 'hidden'} flex-col gap-10 md:gap-0 md:flex-row flex justify-${token ? 'between' : 'end'} items-center min-w-[300px]`} style={{fontFamily: 'Quicksand'}}>
+          {token && <Link to='/' onClick={toggleNavMenu}  className='font-bold tracking-widest'>Home</Link>}
+          {token && <Link to={`${decoded?._id}/profile`} onClick={toggleNavMenu}  className='font-bold tracking-widest'>Profile</Link>}
+          {token ? 
+            <Form action='/logout' method='post'>
+              <button 
+                to='/logout'
+                className='flex flex-row items-center px-5 py-1 rounded-lg bg-red-600 md:shadow-md md:shadow-red-950  ease-in duration-150 hover:bg-red-700 hover:shadow-none  tracking-wide text-md'
+              ><BiPowerOff className='mr-2' />Logout</button></Form> 
+              : <Link to='auth?mode=login'  
+                  className='hidden md:flex flex-row justify-end items-center md:px-5 md:py-1 md:rounded-lg bg-sky-900 md:shadow-lg md:shadow-gray-900 border border-sky-900 ease-in duration-150 hover:bg-sky-900/75 hover:shadow-none md:tracking-widest md:text-lg'
+                ><BiLogInCircle className='md:mr-2' />Signin</Link>
+          }
+        </nav>
+      )}
     </header>
   )
 }
